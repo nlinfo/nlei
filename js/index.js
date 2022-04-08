ee9c47c2644d4c051bc2310d3bdcb913a3f3bab0
Edivaldo <edivaldoluisb16@gmail.com>
2022-04-08 23:39:18 +0100

    update

--- js/index.js
+++ js/index.js
@@ -5,25 +5,25 @@
 //links
 var abrir_fecharLinksRapidos = document.getElementById("ellips_xmark")
 
-var divComLinks = document.querySelector("#divComLinks")
-
 abrir_fecharLinksRapidos.addEventListener("click", abrir_fecharLinks);
 
 
 //função para abrir e fechar a div dos links
 function abrir_fecharLinks() {
+
     var icone_id = document.querySelector('#abrir_fecharLinks')
+    var divComLinks = document.querySelector("#divComLinks")
 
     icone_id.style.color = "red";
     icone_id.classList.toggle("fa-xmark");
 
     if (divComLinks.style.display == "none") {
-        
+
         divComLinks.style.display = "flex"
     } else {
         divComLinks.style.display = "none"
     }
-    
+
 }
 
 
