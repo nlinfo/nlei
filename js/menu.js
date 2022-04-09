ddfbd30f0534d6fa1d40d1359e7e38471e642ff5
Edivaldo <edivaldoluisb16@gmail.com>
2022-04-09 01:47:00 +0100

    menu update

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+
+var botaoMenu = document.querySelector("#botao_menu")
+var menuMobile = document.querySelector('#menu_mobile')
+
+botaoMenu.addEventListener("click", abrir_fecharMenu);
+
+function abrir_fecharMenu() {
+    menuMobile.classList.toggle("menu_mobile");
+    console.log("ativo")
+}
