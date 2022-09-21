7dfaf2e5bfdb8c2486a31cad2a4ee1c7c470e612
Edivaldo <edivaldoluisb16@gmail.com>
2022-09-21 12:03:15 +0100

    loader animation

---
+++ loader_animation/animation.js
@@ -0,0 +1,5 @@
+
+document.querySelector('body').addEventListener('load', ()=>{
+    document.getElementById('loader_animation').style.display = "flex"
+})
+document.getElementById('loader_animation').style.display = "flex"
