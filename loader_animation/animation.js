38565d8e25f534e9c024cf4ed1542169b2480baf
Edivaldo <edivaldoluisb16@gmail.com>
2022-09-21 13:31:52 +0100

    remove loader animation

--- loader_animation/animation.js
+++
@@ -1,5 +0,0 @@
-
-document.querySelector('body').addEventListener('load', ()=>{
-    document.getElementById('loader_animation').style.display = "flex"
-})
-document.getElementById('loader_animation').style.display = "none"
