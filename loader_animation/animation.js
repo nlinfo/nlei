90d5cbd0158bdbf7f2bacbeb95b64f638b35ca69
Edivaldo <edivaldoluisb16@gmail.com>
2022-09-21 12:05:29 +0100

    Update animation.js

--- loader_animation/animation.js
+++ loader_animation/animation.js
@@ -2,4 +2,4 @@
 document.querySelector('body').addEventListener('load', ()=>{
     document.getElementById('loader_animation').style.display = "flex"
 })
-document.getElementById('loader_animation').style.display = "flex"
+document.getElementById('loader_animation').style.display = "none"
